cbedd0cd6bc90c7453fd09f1f8744c76548af967
Austaino <c3ono32@yahoo.com>
2019-10-30 23:55:58 -0700

    Initial push to github

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 app.get("/", (req, res) => {
-  res.send("Hi Prospecting Andelans Hello API");
+  res.send("Hi Prospecting Andelans Hello API this is DevC");
 });
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
