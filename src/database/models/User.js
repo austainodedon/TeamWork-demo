9879aec36b1ae8a7c53b03b1dc9280edd9a9e5bc
Austaino <c3ono32@yahoo.com>
2019-11-26 00:24:52 -0800

    chore(database-setup):database set up set up database with elephant sql folder src setup [finishes]

---
+++ src/database/models/User.js
@@ -0,0 +1,19 @@
+const User = `
+    CREATE TABLE IF NOT EXISTS
+      users(
+        "id" VARCHAR(128) PRIMARY KEY,
+        "firstName" VARCHAR(128) NOT NULL,
+        "lastName" VARCHAR(128) NOT NULL,
+        "email" VARCHAR(128) NOT NULL,
+        "password" VARCHAR(128) NOT NULL,
+        "gender" VARCHAR(128) NOT NULL,
+        "jobRole" VARCHAR(128) NOT NULL,
+        "department" VARCHAR(128) NOT NULL,
+        "address" VARCHAR(128) NOT NULL,
+        "avaterUrl" VARCHAR(128),
+        "userRole" VARCHAR(128),
+        UNIQUE(email)
+      )`;
+export default User;
+
+//id UUID PRIMARY KEY DEFAULT gen_random_uuid()
---
+++ src/database/query/helper.js
@@ -0,0 +1,91 @@
+import dbInterface from "../index";
+import { getColumns, getFields } from "../../utils/db_utils/queryUtil";
+
+// Add item to a table
+export const createItem = async (table, data) => {
+  const values = Object.values(data);
+  const keys = Object.keys(data).map(val => `"${val}"`);
+  const columns = getColumns(keys);
+  const query = {
+    text: `INSERT INTO ${table}(
+      ${keys.toString()}
+    ) VALUES(${columns}) RETURNING *`,
+    values
+  };
+  try {
+    const { rows } = await dbInterface.query(query);
+    return { error: null, result: rows[0] };
+  } catch (error) {
+    return { error: error.message, result: null };
+  }
+};
+
+// UPDATE table_name
+// SET column1 = value1, column2 = value2...., columnN = valueN
+// WHERE[condition];
+
+// Update a single item
+export const updateItem = async (table, id, data) => {
+  const values = Object.values(data);
+  const keys = Object.keys(data).map(val => `"${val}"`);
+  const columns = getColumns(keys);
+  const query = {
+    text: `UPDATE ${table} SET (${keys.toString()}) = (${columns}) WHERE id='${id}' RETURNING *`,
+    values
+  };
+  try {
+    const { rows } = await dbInterface.query(query);
+    return { error: null, result: rows[0] };
+  } catch (error) {
+    return { error: error.message };
+  }
+};
+
+// Delete an item
+export const deleteItem = async (table, id) => {
+  const query = {
+    text: `DELETE FROM ${table} WHERE id=$1 `,
+    values: [id]
+  };
+  try {
+    const { rowCount } = await dbInterface.query(query);
+    return { error: null, result: rowCount };
+  } catch (error) {
+    return { error: error.message };
+  }
+};
+
+// Get a single item from db, expect a single key value pair option
+export const getItem = async (table, option) => {
+  const value = Object.values(option);
+  const key = Object.keys(option)[0];
+  const query = {
+    text: `SELECT * FROM ${table} WHERE ${key}=$1`,
+    values: value
+  };
+  try {
+    const { rows } = await dbInterface.query(query);
+    return { error: null, result: rows[0] };
+    //return { error: null, result: formater(table, rows) };
+  } catch (error) {
+    return { error: error.message };
+  }
+};
+
+// Get items froom the database
+export const getItems = async (table, condition = null) => {
+  const value = condition ? Object.values(condition) : null;
+  const key = condition ? Object.keys(condition).toString() : null;
+  const query = !condition
+    ? { text: `SELECT * FROM ${table}` }
+    : {
+        text: `SELECT * FROM ${table} WHERE "${key}"=$1 `,
+        values: value
+      };
+  try {
+    const { rows } = await dbInterface.query(query);
+    return { error: null, result: rows };
+  } catch (error) {
+    return { error: error.message };
+  }
+};
