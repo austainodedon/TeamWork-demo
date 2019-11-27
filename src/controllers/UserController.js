266d127329af40a76fc0bbcc2a3817ace038624c
Austaino <c3ono32@yahoo.com>
2019-11-27 02:16:44 -0800

    feature(get-current-user):get current user profile login and get current user profile [finishes]

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,9 +1,13 @@
-import uuid from 'uuid/v4';
+import uuid from "uuid/v4";
 import {
-  errorResponse, successResponse, hashPassword, generateToken, successResponsArray
-} from '../utils';
-import { createItem, getItem, getItems } from '../database/query/helper';
-import { comparePassword } from '../utils/bcrypt';
+  errorResponse,
+  successResponse,
+  hashPassword,
+  generateToken,
+  successResponsArray
+} from "../utils";
+import { createItem, getItem, getItems } from "../database/query/helper";
+import { comparePassword } from "../utils/bcrypt";
 
 /**
  * @class UserController
@@ -29,42 +33,49 @@ export default class UserController {
       department,
       address,
       userRole,
-      avaterUrl,
+      avaterUrl
     } = req.body;
 
     // verify user dont already exist
     try {
-      const { result } = await getItem('users', { email });
+      const { result } = await getItem("users", { email });
       if (result) {
-        return errorResponse(res, 409, 'user already exist');
+        return errorResponse(res, 409, "user already exist");
       }
       // remember to update id
-      const { error: createError, result: newUser } = await createItem('users', {
-        id: uuid(),
-        firstName,
-        lastName,
-        password: password ? hashPassword(password) : undefined,
-        email,
-        gender,
-        jobRole,
-        department,
-        address,
-        userRole: userRole ? userRole.toLowerCase() : 'employee',
-        avaterUrl: avaterUrl || 'none',
-      });
+      const { error: createError, result: newUser } = await createItem(
+        "users",
+        {
+          id: uuid(),
+          firstName,
+          lastName,
+          password: password ? hashPassword(password) : undefined,
+          email,
+          gender,
+          jobRole,
+          department,
+          address,
+          userRole: userRole ? userRole.toLowerCase() : "employee",
+          avaterUrl: avaterUrl || "none"
+        }
+      );
       if (createError) {
         throw new Error(createError);
       }
       const { password: ignored, ...rest } = newUser;
-      const token = await generateToken({ userId: rest.id, firstName, lastName });
+      const token = await generateToken({
+        userId: rest.id,
+        firstName,
+        lastName
+      });
       const response = {
         ...rest,
         userId: rest.id,
-        token,
+        token
       };
-      successResponse(res, 201, 'User account successfully created”', response);
+      successResponse(res, 201, "User account successfully created”", response);
     } catch (error) {
-      return errorResponse(res, 500, 'Server error');
+      return errorResponse(res, 500, "Server error");
     }
   }
 
@@ -78,45 +89,66 @@ export default class UserController {
   static async loginUser(req, res) {
     const { password, email } = req.body;
     try {
-      const { result: user } = await getItem('users', { email });
+      const { result: user } = await getItem("users", { email });
       if (user) {
-        const {
-          firstName, id, lastName, password: userPassword
-        } = user;
+        const { firstName, id, lastName, password: userPassword } = user;
         const isPassValid = comparePassword(userPassword, password);
         if (!isPassValid) {
-          return errorResponse(res, 401, 'Authorzation fail');
+          return errorResponse(res, 401, "Authorzation fail");
         }
         const token = await generateToken({ userId: id, firstName, lastName });
         const data = { token, userId: id };
-        return successResponse(res, 200, 'success', data);
+        return successResponse(res, 200, "success", data);
       }
-      return errorResponse(res, 401, 'Authorzation fail');
+      return errorResponse(res, 401, "Authorzation fail");
     } catch (error) {
-      return errorResponse(res, 500, 'Server Error!');
+      return errorResponse(res, 500, "Server Error!");
     }
   }
 
   /**
- * @method getUsers
- * @description - method for admin to get all users
- * @param {object} req - request object
- * @param {object} res - response object
- * @return {object} request response body
- */
+   * @method getUsers
+   * @description - method for admin to get all users
+   * @param {object} req - request object
+   * @param {object} res - response object
+   * @return {object} request response body
+   */
   static async getUsers(req, res) {
     try {
-      const { error, result: users } = await getItems('users');
+      const { error, result: users } = await getItems("users");
       if (error) {
-        return errorResponse(res, 500, 'Internal server error');
+        return errorResponse(res, 500, "Internal server error");
       }
-      const noPasswords = users.map((user) => {
+      const noPasswords = users.map(user => {
         delete user.password;
         return user;
       });
       return successResponsArray(res, 200, noPasswords);
     } catch (error) {
-      return errorResponse(res, 500, 'Server error');
+      return errorResponse(res, 500, "Server error");
+    }
+  }
+  /**
+   * @method getUsers
+   * @description - method  to get current users
+   * @param {object} req - request object
+   * @param {object} res - response object
+   * @return {object} request response body
+   */
+  static async getUser(req, res) {
+    try {
+      const { id } = req.params;
+      const { error, result: user } = await getItems("user", { id });
+      if (error) {
+        return errorResponse(res, 500, "Internal server error");
+      }
+      const noPasswords = user.map(user => {
+        delete user.password;
+        return user;
+      });
+      return successResponsArray(res, 200, noPasswords);
+    } catch (error) {
+      return errorResponse(res, 500, "Server error");
     }
   }
 }
--- src/routes/api/user.js
+++ src/routes/api/user.js
@@ -1,9 +1,9 @@
-import express from 'express';
-import { validate, Authenticate } from '../../middlewares';
-import { UserController } from '../../controllers';
+import express from "express";
+import { validate, Authenticate } from "../../middlewares";
+import { UserController } from "../../controllers";
 
 const { verifyAdmin, verifyToken, verifyRootUser } = Authenticate;
-const { registerUser, loginUser, getUsers } = UserController;
+const { registerUser, loginUser, getUsers, getUser } = UserController;
 
 const router = express.Router();
 
@@ -12,27 +12,44 @@ const router = express.Router();
   @Access: private only admin users
   @Route: <domain>/api/v1/auth/users
 */
-router.get('/users', verifyToken, verifyAdmin, getUsers);
+router.get("/users", verifyToken, verifyAdmin, getUsers);
+/*
+  @Description: endpoint to get current user
+  @Access: private only current users
+  @Route: <domain>/api/v1/auth/user
+*/
+router.get("/user", verifyToken, getUser);
 
 /*
   @Description: endpoint for admin to add user
   @Access: private only admin users
   @Route: <domain>/api/v1/auth/create-user
 */
-router.post('/create-user', validate('userRegister'), verifyToken, verifyAdmin, registerUser);
+router.post(
+  "/create-user",
+  validate("userRegister"),
+  verifyToken,
+  verifyAdmin,
+  registerUser
+);
 
 /*
   @Description: endpoint to create a root user
   @Access: private only request with valid secret
   @Route: <domain>/api/v1/auth/create-user-root
 */
-router.post('/create-user-root', validate('userRegister'), verifyRootUser, registerUser);
+router.post(
+  "/create-user-root",
+  validate("userRegister"),
+  verifyRootUser,
+  registerUser
+);
 
 /*
   @Description: endpoint for admin/employee to sign in
   @Access: prublic any user with valid details
   @Route: <domain>/api/v1/auth/signin
 */
-router.post('/signin', validate('loginUser'), loginUser);
+router.post("/signin", validate("loginUser"), loginUser);
 
 export default router;
